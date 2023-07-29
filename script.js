c3b28dee86288dc9cfd6d11c42e9a756a578b734
Your Name <you@example.com>
2023-07-29 18:08:29 -0400

    added some more images and changed the name

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const imageUrls = [
     'images/blogMeat MKT455.jpg',
     'images/New York. Meat Market 1995 . 2.jpg',
+    'images/64784418.jpg',
     'images/o-90S-NYC-900 (1).jpg',
     'images/o-90S-NYC-900 (2).jpg',
     'images/o-90S-NYC-900.jpg',
+    'images/64784375.jpg',
     'images/tumblr_f201ba4cc5e927e0ca9780a563998b75_93d810b1_1280.jpg'
 ];
 
